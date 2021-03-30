06f7dd8c5f30418b711b9cea37e4d1fdc7740547
Naufal Yukafi <naufalyukafi10@gmail.com>
2021-03-30 09:48:45 +0700

    fix(routes): fixing array resource)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\BarangController;
+use Illuminate\Http\Request;
+/*
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('barang', BarangController::class);
