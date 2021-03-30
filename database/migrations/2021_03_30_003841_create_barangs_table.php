76e408b80d48b801811ed455da81eb3839db7bfd
Naufal Yukafi <naufalyukafi10@gmail.com>
2021-03-30 07:59:10 +0700

    feat(migration): implement table barang

---
+++ database/migrations/2021_03_30_003841_create_barangs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+            $table->char('kodeBarang', 6)->nullable();
+            $table->string('namaBarang', 50)->nullable();
+            $table->string('kategoriBarang', 50)->nullable();
+            $table->integer('harga')->nullable();
+            $table->integer('qty')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barangs');
+    }
+}
